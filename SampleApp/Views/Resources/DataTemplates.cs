d8770eb81cd992d7b97331a8fdcd92c4b5359669
sonnemaf <fons.sonnemans@reflectionit.nl>
2020-06-04 16:15:01 +0200

    small edits

--- SampleApp/Views/Resources/DataTemplates.cs
+++ SampleApp/Views/Resources/DataTemplates.cs
@@ -1,5 +1,7 @@
-﻿namespace SampleApp.Views.Resources {
-    partial class DataTemplates {
+﻿using Windows.UI.Xaml;
+
+namespace SampleApp.Views.Resources {
+    partial class DataTemplates : ResourceDictionary {
         public DataTemplates() {
             this.InitializeComponent();
         }
