fb91b58608598038fe8b1b7b1bc4b8a0c9024206
sonnemaf <fons.sonnemans@reflectionit.nl>
2020-07-09 15:02:15 +0200

    WPF template and SampleWPF app added

---
+++ SampleWpf/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace SampleWpf.Models {
+
+    public class Employee {
+
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee() {
+        }
+
+        public Employee(string name, double salary) {
+            Name = name;
+            Salary = salary;
+        }
+    }
+}
---
+++ SampleWpf/Views/Resources/DataTemplates.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SampleWpf.Views.Resources {
+    partial class DataTemplates : ResourceDictionary {
+        public DataTemplates() {
+            this.InitializeComponent();
+        }
+
+        private void StackPanel_Loaded(object sender, RoutedEventArgs e) {
+            // Some code
+        }
+    }
+}
